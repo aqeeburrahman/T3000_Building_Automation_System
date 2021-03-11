e99503f3a853652d27419b70de69b6d7b00caa3b
Ken <ken@cyberport.com>
2021-03-11 06:44:59 -0600

    Update from main branch

--- T3000/BacnetWeb.cpp
+++ T3000/BacnetWeb.cpp
@@ -42,17 +42,17 @@ BOOL CBacnetWeb::OnInitDialog()
 
     CRect rectClient;
     GetClientRect(&rectClient);
-    if (!m_browser.Create(NULL, NULL, WS_VISIBLE, rectClient, this, 101))
+    /*if (!m_browser.Create(NULL, NULL, WS_VISIBLE, rectClient, this, 101))
     {
         return 0;
-    }
+    }*/
 
     // Initialize the first URL.
     COleVariant noArg;
     //
     //CString strURL("file:///Z:/Temp/1.png");
     CString strURL("https://temcocontrols.com/");
-    m_browser.Navigate(strURL, &noArg, &noArg, &noArg, &noArg);
+//    m_browser.Navigate(strURL, &noArg, &noArg, &noArg, &noArg);
 
 
     return TRUE;  // return TRUE unless you set the focus to a control
